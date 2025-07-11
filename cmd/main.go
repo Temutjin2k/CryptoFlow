cfb40db059833d230084b7c42d69081d1ffa2b45
meruyert4 <meruyertbauyrzhanqyzy@gmail.com>
2025-07-11 21:23:25 +0500

    flags

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,32 @@ package cmd
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"marketflow/config"
 	"marketflow/internal/app"
 	"marketflow/pkg/logger"
+	"os"
 )
 
 func Run() {
 	ctx := context.Background()
+
+	portFlag := flag.Int("port", 0, "Port number")
+	help := flag.Bool("help", false, "Show help message")
+
+	flag.Parse()
+
+	if *help {
+		fmt.Println(`Usage:
+  marketflow [--port <N>]
+  marketflow --help
+
+Options:
+  --port N     Port number
+  --help       Show help message`)
+		os.Exit(0)
+	}
 
 	// Init logger
 	log := logger.InitLogger(ctx, logger.LevelDebug)
@@ -18,6 +37,10 @@ func Run() {
 	if err != nil {
 		log.Error(ctx, "failed to init config", "error", err)
 		return
+	}
+
+	if *portFlag != 0 {
+		cfg.Server.HTTPServer.Port = *portFlag
 	}
 
 	// Printing the config
